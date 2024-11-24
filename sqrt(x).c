aa24a1587ea3e65a4c840bfe6c53df6bf57d6d83
AnushkaTrivedhi161 <anushkatrivedhi23@gmail.com>
2024-11-24 21:12:09 +0530

    Create sqrt(x).c

---
+++ sqrt(x).c
@@ -0,0 +1,20 @@
+int mySqrt(int x) {
+    if(x==0 || x==1)
+    return 0;
+    int l=1,r=x,result=0;
+    while(l<=r)
+    {
+        long long m;
+        m=l+(r-l);
+        if(m*m<=x)
+        {
+            result=m;
+            l=m+1;
+        }
+        else
+        {
+            r=m-1;
+        }
+    }
+    return result;
+}
